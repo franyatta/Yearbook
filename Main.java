cae34a783f11b76cbb96e3df5e3ed744e560c595
franyatta <95552724+franyatta@users.noreply.github.com>
2024-03-13 20:29:36 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ * Francine Vo
+ * CS 252
+ * Student UIN: 01253035
+ * Assignment 6
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student();
+		student.menu();
+
+	}
+
+}
